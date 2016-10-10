6a3fb4d75670291d9c5169d8f15caa6ff8bef09f
dominiccarr <dominiccarr@gmail.com>
2016-10-10 13:52:16 +0000

    added JSON response format and fixed the view name

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -4,6 +4,12 @@ class RandomController < ApplicationController
   def get_random_number
     upper = params[:upper].to_i
     @random = rand(upper)
+    
+    respond_to do |format|
+      format.html 
+      format.json { render :json => @random }
+    end
+
   end
   
 end
